0385353729bda6e949104f8ba4595c94e5693726
ashfilbrun <ashesclaire@gmail.com>
2023-07-19 07:54:17 -0700

    add navbar and app.jsx

---
+++ Tick-ItFront/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import Context from '../Context'
+
+export default function Header () {
+  const 
+
+  return (
+    <div className='navBar'>
+      <ul className='navItems'>
+        <li><Link to="</li>
+      </ul>
+    </div>
+  )
+}
