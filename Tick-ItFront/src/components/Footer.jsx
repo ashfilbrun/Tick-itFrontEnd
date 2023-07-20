eaecd00f7871a8f735d2bc943ff9d2103d951816
ashfilbrun <ashesclaire@gmail.com>
2023-07-20 08:02:10 -0700

    add main base of styling

--- Tick-ItFront/src/components/Footer.jsx
+++ Tick-ItFront/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer () {
   return (
     <div>
       <div>
-        <p className='footer'>This application was collaboratively created and developed by:<a href='https://github.com/ashfilbrun'> &nbsp; Ashlan Filbrun</a>,&nbsp;<a href='https://github.com/elliotvhill'>Elliot Hill</a>,&nbsp;<a href='https://github.com/Tsion-oss'>Tsion Molla</a>| &nbsp;&nbsp; © 2023</p>
+        <p className='footer'>This application was collaboratively created and developed by:&nbsp;<a href='https://github.com/ashfilbrun'>Ashlan Filbrun,</a>&nbsp;<a href='https://github.com/elliotvhill'>Elliot Hill,</a>&nbsp;and&nbsp; <a href='https://github.com/Tsion-oss'>Tsion Molla.</a>&nbsp;&nbsp;© 2023</p>
       </div>
     </div>
   )
