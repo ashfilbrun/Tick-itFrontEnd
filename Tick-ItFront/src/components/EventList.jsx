2a1c96910a8c5cc0881250515d3631386695e64a
Elliot <ellthrowaway2@icloud.com>
2023-07-20 21:26:00 -0400

    fixed axios call -- we have datagit add .

--- Tick-ItFront/src/components/EventList.jsx
+++ Tick-ItFront/src/components/EventList.jsx
@@ -1,22 +1,47 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import * as React from 'react'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 export default function EventList() {
-    const [events, setEvents] = useState
+    const [events, setEvents] = useState([])
     const getEvents = async () => {
-        res = await axios.get('localhost:8000/events')
+        const res = await axios.get('http://127.0.0.1:8000/events')
+        console.log(res)
         setEvents(res)
     }
     useEffect(() => {
         getEvents()
     }, [])
-        
+
+    // let navigate = useNavigate()
+    //     const showEvent = (id) => {
+    //         navigate(`/Event/${id}`)
+    // }
+    
     return events ? (
         <div className="main-container">
             <h2>Event List page</h2>
             <div className="event">
+                {/* {
+                    events.data.map((event) => (
+                        <div key={event.event_name} className="event-info" onClick={() => showEvent(event)}>
+                            <div className="eventBox">
+                                <h3>{event.event_name}</h3>
+                                <ul className="eventUl">
+                                    <li>{event.venue_name}</li>
+                                    <li>{event.event_type}</li>
+                                    <li>Event date: {event.event_date}</li>
+                                    <li>Event time: {event.event_time}</li>
+                                    <li>Ticket price: ${event.ticket_price}</li>
+                                    <li>Tickets left: {event.ticket_quantity}</li>
+                                    <li><button id="addToCart">Add ticket to cart</button></li>
+                                </ul>
+                            </div>
+                            </div>
+                    ) )
+            } */}
+ 
                 <h3>
                     {/* ${Event/:id} */}
                     CREEDENCE CLEARWATER REVIVAL:
