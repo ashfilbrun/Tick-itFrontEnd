2f759925cada9c372708c235431687190f1faa73
Elliot <ellthrowaway2@icloud.com>
2023-07-20 20:30:36 -0400

    fixes

--- Tick-ItFront/src/components/EventList.jsx
+++ Tick-ItFront/src/components/EventList.jsx
@@ -1,8 +1,19 @@
 import { Link } from "react-router-dom"
 import * as React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
-export default function EventList(){
-    return(
+export default function EventList() {
+    const [events, setEvents] = useState
+    const getEvents = async () => {
+        res = await axios.get('localhost:8000/events')
+        setEvents(res)
+    }
+    useEffect(() => {
+        getEvents()
+    }, [])
+        
+    return events ? (
         <div className="main-container">
             <h2>Event List page</h2>
             <div className="event">
@@ -12,9 +23,9 @@ export default function EventList(){
                     Live on tour!</h3>
                 <p>Date</p>
                 <p>Time</p>
-                <p><Link to='/event/:id'>Buy Ticket(s)</Link></p>
+                <p><Link to={`/event/${event.id}`}>Buy Ticket(s)</Link></p>
             </div>
         </div>
-    )
+    ) : <div>Loading...</div>
 }
 
